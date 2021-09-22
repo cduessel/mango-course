5181c813143f5ae663955964e396d244b1cc96a7
christian.dussel <christian.dussel@thoughtworks.com>
2021-09-22 10:28:32 -0300

    refactor: group imports/exports with index files

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,4 +1,4 @@
-import {HttpResponse} from './htttp-response'
+import {HttpResponse} from '.'
 
 export type HttpPostParams<T> = {
   url: string
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,10 @@
-import { HttpPostClientSpy } from '../../tests/mock-http-client'
+import { HttpPostClientSpy } from '../../tests'
 import { RemoteAuthentication } from './remote-authentication'
-import {mockAccountModel, mockAuthentication} from '../../../domain/test/mock-account'
-import {InvalidCredentialsError} from '@/domain/errors/Invalid-credentials-error'
-import {UnexpectedError} from '@/domain/errors/unexpected-error'
-import { HttpStatusCode } from '@/data/protocols/http/htttp-response'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '@/domain/models/account-models'
+import {mockAccountModel, mockAuthentication} from '../../../domain/test'
+import {InvalidCredentialsError, UnexpectedError} from '@/domain/errors'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { AuthenticationParams } from '@/domain/usecases'
+import { AccountModel } from '@/domain/models'
 import faker from 'faker'
 
 type SutTypes = {
