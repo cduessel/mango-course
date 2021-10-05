f447fc238b810157bf037b7d90ed109ca662cbcb
christian.dussel <christian.dussel@thoughtworks.com>
2021-10-05 14:26:38 -0300

    feat: ensure requiredFieldValidation returns error if field is emprty

---
+++ src/validation/required-field/required-field-validation.ts
@@ -0,0 +1,11 @@
+import { RequiredFieldError } from "../errors";
+import { FieldValidation } from "../protocols/field-validation";
+
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error {
+    return new RequiredFieldError()
+  }
+}
