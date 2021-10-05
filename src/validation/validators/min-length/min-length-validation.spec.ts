2d7e8133f1ad965670c29dc27d155af0f2c483bd
christian.dussel <christian.dussel@thoughtworks.com>
2021-10-05 17:00:55 -0300

    test: ensure MinLengthValidation returns falsy when value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,5 +7,11 @@ describe('MinLengthValidation', () => {
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
   })
+
+  test('should return false if value length is valid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('123456')
+    expect(error).toBeFalsy()
+  })
  
 })
