6055850ba054503b6a1219afa5a47e33190661e4
christian.dussel <christian.dussel@thoughtworks.com>
2021-10-05 16:19:51 -0300

    test: ensure EmailValidation don't returns error if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,11 +1,16 @@
 import { InvalidFieldError } from "../../errors"
 import { EmailValidation } from "./email-validation"
+import faker from 'faker'
  
 describe('EmailValidation', () => {
-  test('should return error if email os invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+  test('should return error if email is invalid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
-  
+  test('should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
 })
