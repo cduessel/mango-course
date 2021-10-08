e74cf2720af983a0f06101aebcbc922d04ab4efc
christian.dussel <christian.dussel@thoughtworks.com>
2021-10-08 08:34:13 -0300

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,10 +1,14 @@
-import {RequiredFieldValidation} from '../'
-import { ValidationBuilder } from "./validation-builder"
+import {RequiredFieldValidation, EmailValidation} from '../'
+import { ValidationBuilder as sut } from "./validation-builder"
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
-    const validations = ValidationBuilder.field('any_field').required().build()
+    const validations = sut.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+  test('should return EmailValidation', () => {
+    const validations = sut.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
   
 })
