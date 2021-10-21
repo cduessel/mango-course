50a1c3d38711dc18f321be983dd6fa648d3e4172
christian.dussel <christian.dussel@thoughtworks.com>
2021-10-21 11:51:16 -0300

    refactor: move factories to their own files

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -3,13 +3,10 @@ import {Login} from '../../../../presentation/pages'
 import { ValidationComposite } from '../../../../validation/validators/'
 import {ValidationBuilder} from '../../../../validation/validators/builder/validation-builder'
 import {makeRemoteAuthentication} from '../../usecases/authentication/remote-authentication-factory'
+import { makeLoginValidation } from "./login-validation-factory"
 
 export const makeLogin: React.FC = () => {
-  const validationComposite = new ValidationComposite([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
-  ])
   return (
-    <Login authentication={makeRemoteAuthentication()} validation={validationComposite} />
+    <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()} />
   )
 }
